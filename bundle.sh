e5c295dd7237abde8a8da60f186d863aefa08531
Delfi-CH <delfi@delfi.dev>
2026-05-13 16:48:59 +0200

    feat: do most of the debian bundeling

--- bundle.sh
+++ bundle.sh
@@ -28,7 +28,7 @@ if [ "$status" != 0 ]; then
     echo "Bundeling backend failed!"
     exit 1
 else
-    echo "Bundeling backend was successfull!"
+    echo "Bundeling backend was sucessfull!"
 fi
 
 echo "Bundeling frontend..."
@@ -38,7 +38,7 @@ if [ "$status" != 0 ]; then
     echo "Bundeling frontend failed!"
     exit 1
 else
-    echo "Bundeling frontend was successfull!"
+    echo "Bundeling frontend was sucessfull!"
 fi
 echo "Copying frontend to dist..."
 cp -r ui dist/ui
@@ -47,5 +47,86 @@ if [ "$status" != 0 ]; then
     echo "Copying frontend to dist failed!"
     exit 1
 else
-    echo "Copying frontend to dist was successfull!"
+    echo "Copying frontend to dist was sucessfull!"
+fi
+
+bundle_debian () {
+    echo "Testing for dpkg-deb..."
+    type dpkg-deb &> /dev/null
+    status=$?
+    if [ "$status" != 0 ]; then
+        echo "dpkg-deb wasnt found in \$PATH!"
+        echo "Aborting..."
+        exit 1
+    fi
+    echo "dpkg-deb was found!"
+
+    echo "Copying files..."
+
+    cp dist/httpstart dist/httpstart-debian/usr/bin/httpstart
+    status=$?
+    if [ "$status" != 0 ]; then
+        echo "Copying files failed!"
+        exit 1
+    fi
+
+    cp dist/index.cjs dist/httpstart-debian/usr/share/httpstart/index.cjs
+    status=$?
+    if [ "$status" != 0 ]; then
+        echo "Copying files failed!"
+        exit 1
+    fi
+
+    cp -r dist/ui dist/httpstart-debian/usr/share/httpstart/
+    status=$?
+    if [ "$status" != 0 ]; then
+        echo "Copying files failed!"
+        exit 1
+    fi
+    
+    echo "Copying files was sucessfull!"
+
+    echo "Copying metadata..."
+
+    pushd dist/httpstart-debian &> /dev/null
+    gzip -9 -n -k DEBIAN/changelog &> /dev/null
+    status=$?
+    if [ "$status" != 0 ]; then
+        echo "Copying metadata failed!"
+        exit 1
+    fi
+
+    mv DEBIAN/changelog.gz ./usr/share/doc/node-httpstart/
+    status=$?
+    if [ "$status" != 0 ]; then
+        echo "Copying metadata failed!"
+        exit 1
+    fi
+    rm -f DEBIAN/changelog.gz
+
+    cp DEBIAN/copyright ./usr/share/doc/node-httpstart/
+    status=$?
+    if [ "$status" != 0 ]; then
+        echo "Copying metadata failed!"
+        exit 1
+    fi
+    popd &> /dev/null
+
+
+    echo "Building .deb file..."
+
+    pushd dist &> /dev/null 
+    dpkg-deb --root-owner-group --build httpstart-debian/ &> /dev/null
+    status=$?
+    if [ "$status" != 0 ]; then
+        echo "Building .deb file failed!"
+        exit 1
+    fi
+    popd &> /dev/null
+    echo "Building .deb file was sucessfull!"
+}
+
+if [ "$type" == "deb" ]; then
+    echo "Bundeling for Debian (dpkg)..."
+    bundle_debian
 fi
---
+++ clean.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+# 13.05.2026 Delfi-CH
+
+# Description
+# Script for cleaning up the bundeling of the linux-web-installer
+
+# Params:
+# type (options: deb, rpm, pacman, tgz)
+
+# Usage:
+# Best invoked via package.json
+# ./clean.sh (type)
+
+# Returns:
+# 0: All good
+# 1: Error
+
+type=$1
+if [ -z "$type" ]; then
+    echo "No type set, defaulting to tar.gz..."
+fi
+
+echo "Deleting shared files..."
+
+rm -f dist/index.cjs
+status=$?
+if [ "$status" != 0 ]; then
+    echo "Deleting shared files failed!"
+    exit 1
+fi
+
+rm -rf dist/ui
+status=$?
+if [ "$status" != 0 ]; then
+    echo "Deleting shared files failed!"
+    exit 1
+fi
+
+echo "Deleting shared files was sucessfull!"
+
+if [ "$type" == "deb" ]; then
+    echo "Deleting debian specific files..."
+
+    rm -f dist/httpstart-debian/usr/bin/httpstart
+    status=$?
+    if [ "$status" != 0 ]; then
+        echo "Deleting debian specific files failed!"
+        exit 1
+    fi
+
+    rm -rf dist/httpstart-debian/usr/share/httpstart/*
+    status=$?
+    if [ "$status" != 0 ]; then
+        echo "Deleting debian specific files failed!"
+        exit 1
+    fi
+
+    rm -rf dist/httpstart-debian/usr/share/doc/node-httpstart/*
+    status=$?
+    if [ "$status" != 0 ]; then
+        echo "Deleting debian specific files failed!"
+        exit 1
+    fi
+
+    rm -f dist/httpstart-debian.deb
+    status=$?
+    if [ "$status" != 0 ]; then
+        echo "Deleting debian specific files failed!"
+        exit 1
+    fi
+
+    echo "Deleting debian specific files was sucessfull!"
+
+    exit 0
+fi
