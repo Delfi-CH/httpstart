41d4924f9b9453139da6a8ee479fb354947b4025
Delfi-CH <delfi@delfi.dev>
2026-05-13 13:52:57 +0200

    chore: add script scaffhold for bundle and launcher

---
+++ bundle.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# 13.05.2026 Delfi-CH
+
+# Description
+# Script for bundeling the linux-web-installer
+
+# Params:
+# type (options: deb, rpm, pacman, tgz)
+
+# Usage:
+# Best invoked via package.json
+# ./bundle.sh (type)
+
+# Returns:
+# 0: All good
+# 1: Error
