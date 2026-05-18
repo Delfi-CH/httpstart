afe911e844068e0763bac9d7013a4c009e83d3e4
Delfi-CH <delfi@delfi.dev>
2026-05-18 10:34:12 +0200

    feat: conditional themeing with light/dark/ubuntu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,33 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
 	import Navbar from '$lib/components/Navbar.svelte';
+    import { Distribution } from '$lib/data';
+    let { data, children } = $props();
 
-	let { children } = $props();
 	const mode = import.meta.env.MODE;
+
+	const getDistro = () => data.distro.distro
+	const distro = getDistro();
+	const darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+	let cssUrl = "/ui/css/light/bootstrap.min.css"
+
+	if (mode === "development" && distro === Distribution.Ubuntu) {
+		cssUrl = "/css/ubuntu/bootstrap.min.css"
+	} else if (mode === "development" && darkMode) {
+		cssUrl = "/css/dark/bootstrap.min.css"
+	} else if (mode === "development") {
+		cssUrl = "/css/light/bootstrap.min.css"
+	} else if (distro === Distribution.Ubuntu) {
+		cssUrl = "/ui/css/ubuntu/bootstrap.min.css"
+	} else if (darkMode) {
+		cssUrl = "/ui/css/dark/bootstrap.min.css"
+	}
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
-	{#if mode === "development"}
-		<link rel="stylesheet" href="/bootstrap.min.css"/>
-	{:else}
-		<link rel="stylesheet" href="/ui/bootstrap.min.css"/>
-	{/if}
-	</svelte:head>
+	<link rel="stylesheet" href={cssUrl}/>
+</svelte:head>
 
 <Navbar></Navbar>
 
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,19 @@
 export const ssr = false
 export const prerender = true
+
+import axios from 'axios'
+
+export async function load() {
+    const mode = import.meta.env.MODE;
+
+	let serverURL = "/"
+	if (mode === "development") {
+        serverURL = "http://localhost:29222";
+    }
+
+	const distro = (await axios.get(serverURL +'/api/distro')).data
+
+	return {
+		distro
+	}
+}
